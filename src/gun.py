fff8b87ee0a6d71a27fb0f5dc155f09481af7601
Nateheff <nateheffgk05@gmail.com>
2024-09-05 17:19:46 -0700

    Added GPIO instructions for motor controller

--- src/gun.py
+++ src/gun.py
@@ -3,22 +3,36 @@ from gpiozero import OutputDevice
 import RPi.GPIO as GPIO
 
 
-motor_speed = None
+load_speed = None
 
 def gun_init():
     GPIO.setmode(GPIO.BOARD)
 
+    GPIO.setup(12, GPIO.OUT)
+
+    GPIO.setup(36, GPIO.OUT)
     GPIO.setup(38, GPIO.OUT) #load motor
     GPIO.setup(40, GPIO.OUT) #fire motor
+
+    load_speed = GPIO.PWM(36, 60)
+    load_speed.start(0)
     
 
 def fire():
+    GPIO.output(12, True)
+
     GPIO.output(40, True)
-    GPIO.output(38, True)
+    GPIO.output(38, False)
+    load_speed.ChangeDutyCycle(100)
 
 
 def cease_fire():
     GPIO.output(38, False)
     GPIO.output(40, False)
+
+    GPIO.output(12, False)
+
+    load_speed.ChangeDutyCycle(0)
+    load_speed.stop(0)
 
     GPIO.cleanup()
